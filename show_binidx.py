8f6aa434b8bf54a33d99f7c3cadd1966737d204d
YuChuXi <cyu245656@gmail.com>
2025-03-24 23:34:21 +0800

    pn

--- show_binidx.py
+++ show_binidx.py
@@ -1,17 +1,21 @@
+import numpy
 from rwkvt.dataset.binidx import MMapIndexedDataset
-
-
 from tokenizer.rwkv_tokenizer import TRIE_TOKENIZER
 from rwkvt.dataset.binidx import MMapIndexedDataset
 
 tokenizer = TRIE_TOKENIZER("tokenizer/rwkv_vocab_v20230424.txt")
 
-dataset = MMapIndexedDataset('data/message_cache')
-print(f'Total documents: {len(dataset)}')
+dataset = MMapIndexedDataset("data/message_cache")
+print(f"Total documents: {len(dataset)}")
 
-for i in range(0, 50000, 1000):
-    tokens = dataset[i].astype(int)
-    print(f'\nSample {i+1}:')
-    print('Token IDs:', tokens)
-    print('Decoded text:', tokenizer.decode(tokens.tolist()))
-    print('Token IDs:', tokens)
+with open("data/dump.txt", "wb") as f:
+    for i in range(0, len(dataset), 100):
+        tokens: numpy.ndarray = dataset[i].astype(int)
+        tokens = numpy.where(tokens == 65530, 62, tokens)
+        tokens = numpy.where(tokens == 24, 261, tokens)
+        string = tokenizer.decodeBytes(tokens.tolist())
+        print(f"\nSample {i+1}:")
+        print("Decoded text:", string.decode(errors="ignore"))
+        print("Token IDs:", tokens)
+        f.write(string)
+        f.write(b"\n\n" + b"=" * 80 + b"\n\n")
